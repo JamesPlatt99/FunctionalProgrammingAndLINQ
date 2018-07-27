fa267852c66b2922dbae156b9f1a5e5e480c82da
James Platt <James.Platt@Neptune.com>
2018-07-27 14:41:54 +0100

    Added unit tests for new solution

--- UnitTests/CheckMaxConsecRedNotPoisonedTests.cs
+++ UnitTests/CheckMaxConsecRedNotPoisonedTests.cs
@@ -20,8 +20,11 @@ namespace UnitTests
             var maxConsecRedNotPoisonedForEach = new MaxConsecRedNotPoisoned(apples, MaxConsecRedNotPoisoned.Methods.ForEach);
             var maxConsecRedNotPoisonedLinq = new MaxConsecRedNotPoisoned(apples, MaxConsecRedNotPoisoned.Methods.LINQ);
             var maxConsecRedNotPoisonedLinq2 = new MaxConsecRedNotPoisoned(apples, MaxConsecRedNotPoisoned.Methods.LINQ2);
+            var maxConsecRedNotPoisonedLinq3 = new MaxConsecRedNotPoisoned(apples, MaxConsecRedNotPoisoned.Methods.LINQ3);
 
-            Assert.AreEqual(maxConsecRedNotPoisonedForEach.Result, maxConsecRedNotPoisonedLinq.Result, maxConsecRedNotPoisonedLinq2.Result);
+            Assert.AreEqual(maxConsecRedNotPoisonedForEach.Result, maxConsecRedNotPoisonedLinq.Result);
+            Assert.AreEqual(maxConsecRedNotPoisonedLinq2.Result, maxConsecRedNotPoisonedLinq3.Result);
+            Assert.AreEqual(maxConsecRedNotPoisonedForEach.Result, maxConsecRedNotPoisonedLinq2.Result);
         }
 
         [TestMethod]
@@ -51,6 +54,16 @@ namespace UnitTests
             List<Apple> apples = applePicker.PickApples().Take(10000).ToList();
 
             var maxConsecRedNotPoisoned = new MaxConsecRedNotPoisoned(apples, MaxConsecRedNotPoisoned.Methods.LINQ2);
+            Assert.AreEqual(maxConsecRedNotPoisoned.Result, 10);
+        }
+
+        [TestMethod]
+        public void CheckMaxConsecNotPoisonedLinq3()
+        {
+            var applePicker = new ApplePicker();
+            List<Apple> apples = applePicker.PickApples().Take(10000).ToList();
+
+            var maxConsecRedNotPoisoned = new MaxConsecRedNotPoisoned(apples, MaxConsecRedNotPoisoned.Methods.LINQ3);
             Assert.AreEqual(maxConsecRedNotPoisoned.Result, 10);
         }
     }
